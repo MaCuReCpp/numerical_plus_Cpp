5734cdb7a966d495c689e66bbe73988ee38f16cc
MaCuRe <rwcd62hsdt@privaterelay.appleid.com>
2026-09-23 17:07:31 +0900

    Add Root.hpp with Bisection and Secant function declarations

---
+++ Root.hpp
@@ -0,0 +1,9 @@
+#ifndef GUARD_ROOT_H
+#define GUARD_ROOT_H
+
+#include <functional>
+
+double Bisection(std::function<double(double)>, double, double);
+double Secant(std::function<double(double)>, double, double);
+
+#endif
